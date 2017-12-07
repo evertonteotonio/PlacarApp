83d342eff66c5bfa2e84971d0907377f26e386f0
logonrm <logonrm@fiap.com.br>
2017-12-06 22:04:17 -0200

    Segunda Aula de Android

--- app/src/main/java/br/com/fiap/placarapp/GameActivity.java
+++ app/src/main/java/br/com/fiap/placarapp/GameActivity.java
@@ -2,6 +2,7 @@ package br.com.fiap.placarapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
@@ -12,22 +13,65 @@ public class GameActivity extends AppCompatActivity {
     private TextView tvTimeCasa;
     private TextView tvTimeVisitante;
 
+    private TextView tvPlacarCasa;
+    private TextView tvPlacarVisitante;
+
+    private int golCasa;
+    private int golVisitante;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        tvTimeCasa = (TextView) findViewById(R.id.tvTimeCasa);
-        tvTimeVisitante = (TextView) findViewById(R.id.tvTimeVisitante);
+        tvTimeCasa = (TextView) findViewById(R.id.tvTimeCasaValue);
+        tvTimeVisitante = (TextView) findViewById(R.id.tvTimeVisitanteValue);
 
+        tvPlacarCasa = (TextView) findViewById(R.id.tvPlacarCasa);
+        tvPlacarVisitante = (TextView) findViewById(R.id.tvPlacarVisitante);
 
-
-        if (getIntent() != null){
+        if (getIntent() != null) {
             timeCasa = getIntent().getStringExtra("TIMECASA");
             timeVisitante = getIntent().getStringExtra("TIMEVISITANTE");
 
             tvTimeCasa.setText(timeCasa);
             tvTimeVisitante.setText(timeVisitante);
         }
+
+        if (savedInstanceState != null) {
+
+            golCasa = savedInstanceState.getInt("GOLCASA");
+            golVisitante = savedInstanceState.getInt("GOLVISITANTE");
+
+            tvPlacarCasa.setText(String.valueOf(golCasa));
+            tvPlacarVisitante.setText(String.valueOf(golVisitante));
+        }
+
+    }
+
+    public void golCasa(View v) {
+        golCasa++;
+        tvPlacarCasa.setText(String.valueOf(golCasa));
+    }
+
+    public void golVisitante(View v) {
+        golVisitante++;
+        tvPlacarVisitante.setText(String.valueOf(golVisitante));
+    }
+
+    public void limpaPlacar(View v) {
+        golCasa = 0;
+        golVisitante = 0;
+
+        tvPlacarCasa.setText(String.valueOf(golCasa));
+        tvPlacarVisitante.setText(String.valueOf(golVisitante));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("GOLCASA", golCasa);
+        outState.putInt("GOLVISITANTE", golVisitante);
     }
 }
